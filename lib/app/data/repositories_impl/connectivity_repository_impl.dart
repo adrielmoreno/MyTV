9c96a75da385aa07ca3981b18c9978b02dc57caa
MorenoCreativo <adrielmoreno@hotmail.com>
2025-03-24 09:21:08 +0100

    Añadir permisos de conexión a Internet y ubicación en el manifiesto, mejorar el manejo de errores en el repositorio de conectividad, implementar servicio de almacenamiento seguro y ajustar el archivo pubspec.yaml

--- lib/app/data/repositories_impl/connectivity_repository_impl.dart
+++ lib/app/data/repositories_impl/connectivity_repository_impl.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 
 import '../../domain/repositories/connectivity_repository.dart';
@@ -6,6 +8,12 @@ class ConnectivityRepositoryImpl implements ConnectivityRepository {
   final InternetConnection _internetConnection;
   ConnectivityRepositoryImpl(this._internetConnection);
   @override
-  Future<bool> get hasIntenert async =>
-      await _internetConnection.hasInternetAccess;
+  Future<bool> get hasIntenert async {
+    try {
+      return await _internetConnection.hasInternetAccess;
+    } catch (e) {
+      log(e.toString(), name: 'ConnectivityRepositoryImpl');
+      return false;
+    }
+  }
 }
