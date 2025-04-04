e1cb8424d1370c6c37334da9d804b0b9297d5af3
MorenoCreativo <adrielmoreno@hotmail.com>
2025-04-04 14:59:33 +0200

    Refactor main.dart to use path URL strategy; update pubspec.yaml and pubspec.lock for dependency adjustments

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'app/core/di/app_modules.dart';
 import 'app/data/services/local/secure_storage_service.dart';
@@ -7,7 +8,7 @@ import 'app/my_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  usePathUrlStrategy();
   AppModules().setup();
 
   await inject.get<SecureStorageService>().init();
